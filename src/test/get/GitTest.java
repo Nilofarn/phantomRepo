333d30b3e485cc772db7062bbd636dcf499bc75d
Nilofarn <nilofar.nawabi@tekschool.us>
2023-02-23 13:44:01 -0500

    Push code

--- src/test/get/GitTest.java
+++ src/test/get/GitTest.java
@@ -1,5 +1,11 @@
 package test.get;
 
 public class GitTest {
-
+public void test() {
+	System.out.println("this is Git test");
 }
+	public void testTwo() {
+System.out.println("this is class practice");		
+	}
+}
+
